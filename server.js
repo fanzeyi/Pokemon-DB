9460cbb9f7533db7a23500b726db3300f0614297
barathnadar <nadar.barath18@siesgst.ac.in>
2019-12-05 22:53:56 +0530

    added home

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ function base64_encode(file) {
   var bitmap = fs.readFileSync(file);
   return new Buffer.from(bitmap).toString("base64");
 }
+function findid(id){
+  if (id < 10) return "00" + id;
+  else if (id < 100) return "0" + id;
+  else return id;
+}
 //-----------------------------------------------//
 const Pokemondata = JSON.parse(fs.readFileSync("./pokedex.json", "utf-8"));
 //--------------MiddleWares---------------------//
@@ -17,27 +22,47 @@ const getpokemondata = (req, res) => {
   const name = req.params.pid;
   console.log(name);
   const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
-  const Pokemon = Pokemondata.find(el => el.name.english === nameCapitalized);
+  var Pokemon = Pokemondata.find(el => el.name.english === nameCapitalized);
   if (!Pokemon)
     return res
       .status(404)
       .json({ status: "failed", message: "Data not found" });
-  var id = Pokemon.id;
-
-  if (id < 10) id = "00" + id;
-  else if (id < 100) id = "0" + id;
+  var id = findid(Pokemon.id);
   console.log(id);
+  Pokemon={...Pokemon,image: "http://www.serebii.net/pokemongo/pokemon/" + id+".png",}
   res.status(200).json({
     status: "success",
-    image: "http://www.serebii.net/pokemongo/pokemon/" + id+".png",
     data: {
       Pokemon
     }
   });
 };
-
+const pokemonrandomizer =(req,res)=>{
+  var pokemon =[] ;
+  
+  for (let i=0; i<10; i+=1) {
+    var temppokemon = Pokemondata[Math.floor(Math.random() * 809)]
+    var id = findid(temppokemon.id);
+    temppokemon = {...temppokemon,image :"http://www.serebii.net/pokemongo/pokemon/" +id+".png",}
+    pokemon.push(temppokemon);
+   }
+   return pokemon;
+}
+const home=(req,res)=>{
+ const pokemon = pokemonrandomizer();
+  res.status(200)
+  .json({
+    status: "success",
+    data: {
+      pokemon
+    }
+  });
+}
 //----------------------------------------------//
 pokemonroute.route("/:pid").get(getpokemondata);
+pokemonroute.route("/").get(home);
+app.route("/").get(home);
+
 //-------------------Server---------------------//
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
