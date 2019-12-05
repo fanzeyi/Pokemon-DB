9d00fffeffd5e719e993af2549dd150e833820a5
barathnadar <nadar.barath18@siesgst.ac.in>
2019-12-05 17:11:18 +0530

    Added Server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+//-------------------Packages---------------------//
+const express = require("express");
+const fs = require ("fs")
+const app = express();
+const pokemonroute = express.Router();
+app.use('/pokemon', pokemonroute);
+app.use(express.json());
+
+//-----------------------------------------------//
+const Pokemondata = JSON.parse(fs.readFileSync("./pokedex.json", "utf-8"));
+//--------------MiddleWares---------------------//
+const getpokemondata= (req,res)=>{
+    const name = req.params.pid;
+    console.log(name)
+    const Pokemon = Pokemondata.find(el => el.name.english === name); 
+    
+    res.status(200)
+    .json({
+      status: 'success',
+      image: __dirname+'\\images\\'+Pokemon.id,
+      data: {
+        Pokemon
+      }
+    });
+}
+
+//----------------------------------------------//
+pokemonroute
+.route('/:pid')
+.get(getpokemondata);
+//-------------------Server---------------------//
+app.listen(3000, "localhost", () => {
+  console.log("listening ");
+});
+//-----------------------------------------------//
