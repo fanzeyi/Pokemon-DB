562b93fc0914ad33bd547af0a6b9971f4754ed15
barathnadar <nadar.barath18@siesgst.ac.in>
2019-12-07 17:42:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const pokemonroute = express.Router();
 app.use("/pokemon", pokemonroute);
 app.use(express.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 //-----------------------------------------------//
 
 function findid(id) {
