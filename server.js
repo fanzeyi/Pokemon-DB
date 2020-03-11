01b2a94f4b7bb0e210eb78abd475206a57709bee
Barath Nadar <42098783+barath121@users.noreply.github.com>
2020-03-11 17:53:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 pokemonroute.use(cors());
 pokemonroute.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials','true');
+  res.header('Access-Control-Allow-Credentials','omit');
   next();
 });
 //-----------------------------------------------//
